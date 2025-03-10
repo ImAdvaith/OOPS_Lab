501d4743a5f5c9bbcd4381bafac12d43cdeac02b
Advaith S <73543162+ImAdvaith@users.noreply.github.com>
2025-03-10 20:09:54 +0530

    Create Bank_Management.java
    Bank Account Management System

---
+++ Java Programs/Bank_Management.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+// Base class
+class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) System.out.println("Insufficient balance!");
+        else {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        }
+    }
+
+    public void displayAccount() {
+        System.out.println("Account Holder: " + accountHolder + " | Balance: " + balance);
+    }
+}
+
+// Derived classes
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+}
+
+class CurrentAccount extends BankAccount {
+    public CurrentAccount(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+}
+
+public class BankSystem {
+    public static void main(String[] args) {
+        SavingsAccount sa = new SavingsAccount("Advaith", 5000);
+        sa.deposit(2000);
+        sa.withdraw(3000);
+        sa.displayAccount();
+    }
+}
