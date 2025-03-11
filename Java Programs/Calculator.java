e502e430dfc5563f402513e6703f587c32c7972a
Advaith S <73543162+ImAdvaith@users.noreply.github.com>
2025-03-11 08:00:31 +0530

    Example program for method overloading

---
+++ Java Programs/Calculator.java
@@ -0,0 +1,24 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of 2 numbers: " + calc.add(5, 10));
+        System.out.println("Sum of 3 numbers: " + calc.add(5, 10, 15));
+        System.out.println("Sum of 2 double numbers: " + calc.add(5.5, 2.2));
+    }
+}
