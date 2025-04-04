bee00746bd855629dd4e432e5b99bf19a4e5d769
Advaith S <hestichero@gmail.com>
2025-04-04 09:44:48 +0530

    Create TicketManager.java

---
+++ Packages/User Defined/Ticket_Program/ticket/TicketManager.java
@@ -0,0 +1,36 @@
+package ticket;
+
+import java.util.ArrayList;
+import java.io.*;
+
+public class TicketManager {
+    private ArrayList<Ticket> ticketList;
+    private static final String FILE_NAME = "tickets.txt";
+
+    public TicketManager() {
+        ticketList = new ArrayList<>();
+    }
+
+    public void bookTicket(String name, String destination, int seatNumber) {
+        Ticket ticket = new Ticket(name, destination, seatNumber);
+        ticketList.add(ticket);
+        saveToFile(ticket);
+    }
+
+    public void showAllTickets() {
+        System.out.println("\n All Booked Tickets:");
+        for (Ticket t : ticketList) {
+            System.out.println(t);
+        }
+    }
+
+    private void saveToFile(Ticket ticket) {
+        try (FileWriter fw = new FileWriter(FILE_NAME, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write(ticket.toCSV());
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println("Could not save ticket.");
+        }
+    }
+}
