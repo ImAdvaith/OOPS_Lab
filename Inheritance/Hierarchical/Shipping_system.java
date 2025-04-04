17754d693bc5b1b69adeef3469d21ab0ff0ef8e4
Advaith S <hestichero@gmail.com>
2025-04-04 19:51:35 +0530

    Create Shipping_system.java

---
+++ Inheritance/Hierarchical/Shipping_system.java
@@ -0,0 +1,107 @@
+abstract class Shipment {
+    String trackingNumber;
+    double weight;
+
+    Shipment(String trackingNumber, double weight) {
+        this.trackingNumber = trackingNumber;
+        this.weight = weight;
+    }
+
+   
+    abstract double calculateCost();
+    abstract int estimatedDeliveryTime(); 
+
+    
+    void trackShipment() {
+        System.out.println("Tracking Shipment: " + trackingNumber);
+    }
+}
+
+class AirShipment extends Shipment {
+    AirShipment(String trackingNumber, double weight) {
+        super(trackingNumber, weight);
+    }
+
+    @Override
+    double calculateCost() {
+        return weight * 5.0; 
+    }
+
+    @Override
+    int estimatedDeliveryTime() {
+        return 3; 
+    }
+
+    void expressShipping() {
+        System.out.println("Air shipment executed for faster delivery.");
+    }
+}
+
+class SeaShipment extends Shipment {
+    SeaShipment(String trackingNumber, double weight) {
+        super(trackingNumber, weight);
+    }
+
+    @Override
+    double calculateCost() {
+        return weight * 2.0; 
+    }
+
+    @Override
+    int estimatedDeliveryTime() {
+        return 15; 
+    }
+
+   
+    void oceanFreight() {
+        System.out.println("Sea shipment is part of ocean freight.");
+    }
+}
+
+
+class GroundShipment extends Shipment {
+    GroundShipment(String trackingNumber, double weight) {
+        super(trackingNumber, weight);
+    }
+
+    @Override
+    double calculateCost() {
+        return weight * 3.0; 
+    }
+
+    @Override
+    int estimatedDeliveryTime() {
+        return 7; 
+    }
+
+    
+    void deliveryConfirmation() {
+        System.out.println("Ground shipment requires delivery confirmation.");
+    }
+}
+
+public class HierarchicalInheritanceExample {
+    public static void main(String[] args) {
+        e
+        AirShipment air = new AirShipment("A123", 10.0);
+        SeaShipment sea = new SeaShipment("S456", 50.0);
+        GroundShipment ground = new GroundShipment("G789", 30.0);
+
+       
+        displayShipmentDetails(air);
+        displayShipmentDetails(sea);
+        displayShipmentDetails(ground);
+
+        System.out.println();
+
+        air.expressShipping();
+        sea.oceanFreight();
+        ground.deliveryConfirmation();
+    }
+
+    static void displayShipmentDetails(Shipment shipment) {
+        shipment.trackShipment();
+        System.out.println("Cost: $" + shipment.calculateCost());
+        System.out.println("Estimated Delivery Time: " + shipment.estimatedDeliveryTime() + " days");
+    }
+}
